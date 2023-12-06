190d2e68cab8f28a517ae948347074de94388761
Piotr Justyna <piotr.justyna@gmail.com>
2023-12-06 12:19:42 +0000

    configured aws cli

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,19 @@
 #!/bin/zsh
 
+# --- input ------------------------------------ #
+
 WORKING_DIRECTORY=$1
 
+# --- configuring git -------------------------- #
+
 git config --global --add safe.directory "$WORKING_DIRECTORY"
+
+# --- configuring aws cli ---------------------- #
+
+aws configure set aws_access_key_id S3RVER
+
+aws configure set aws_secret_access_key S3RVER
+
+aws configure set region us-east-1
 
 /bin/zsh
